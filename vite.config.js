828d385546dae19b745f0c8bc40a43ce3442bfa8
Abootlha <104060073+Abootlha@users.noreply.github.com>
2024-06-28 01:06:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Manually split vendor chunk
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    // Increase chunk size warning limit to 1000 kB
+    chunkSizeWarningLimit: 1000,
+    // Optional: specify output directory
+    outDir: 'dist',
+  },
+});
